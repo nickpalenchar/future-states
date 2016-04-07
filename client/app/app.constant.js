966f66b027f9268c800723408ea5567297d659aa
Nick <npalenchar@gmail.com>
2016-04-07 12:42:51 -0400

    initial commit

---
+++ client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function(angular, undefined) {
+'use strict';
+
+angular.module('programmingWorkApp.constants', [])
+
+.constant('appConfig', {userRoles:['guest','user','admin']})
+
+;
+})(angular);
